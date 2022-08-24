aa95c51259db370f85d809b7b8a6318ecbd3f1e4
scooo <scooomaker@foxmail.com>
2022-08-24 12:56:18 +0800

    third

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 export default function Home() {
   return (
     <>
-
-      <main>
-
-        Hello World!
-        <meta http-equiv="refresh" content="15; URL='https://sdfz.fjnu.edu.cn/'" />
-      </main>
+       <meta httpEquiv="refresh" content="0; URL='https://sdfz.fjnu.edu.cn/'" />
     </>
   )
 }
